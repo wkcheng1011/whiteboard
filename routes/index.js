7010b2f32393a81ad58025ec91eb5472ba03cf48
Ivan Cheng <wkcheng1011@gmail.com>
2021-03-30 09:12:45 +0800

    Remove unnecessary argument

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const { Database } = require("sqlite3");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     if (!req.session.user) {
         res.redirect("/login");
     } else {
-        res.render("index", { title: "Welcome!" });
+        res.render("index");
     }
 });
 
