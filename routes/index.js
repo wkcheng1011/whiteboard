4e74c55705f1c01bb0a0c7bdaf582292e4a27634
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-02 17:22:20 +0800

    Add random quote function, expose tasks to frontend

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const fetch = require("node-fetch");
+const { Database } = require("sqlite3");
+let db = new Database("");
+
 /* GET home page. */
 router.get("/", async (req, res) => {
-    if (!req.session.user) {
-        res.redirect("/login");
-    } else {
-        res.render("index");
-    }
+	if (!req.session.user) {
+		res.redirect("/login");
+	} else {
+        db = res.locals.db;
+
+		const quote = await (await fetch("https://api.quotable.io/random")).json();
+        const tasks = await db.all("select tasks.id as task_id, tasks.name as task_name, classes.name as class_name, start, end from tasks, classes where tasks.class_id = classes.id");
+        
+		res.render("index", { quote, tasks });
+	}
 });
 
 module.exports = router;
