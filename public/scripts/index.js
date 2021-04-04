952e884209e1e2fda298582006c27ccbb9d6e121
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-04 19:56:14 +0800

    Split index page script into individual file

---
+++ public/scripts/index.js
@@ -0,0 +1,33 @@
+function nameSorter(a, b) {
+    return a.replace(/href=.*"/, "").localeCompare(b.replace(/href=.*"/, ""));
+}
+
+// Format time
+const current = new Date();
+const times = document.querySelectorAll(".time");
+for (const time of times) {
+    const diff = Math.abs((current - new Date(time.textContent)) / 1000);
+    let magnitude, unit;
+    if (diff > 86400) { // Over a day
+        magnitude = diff / 86400;
+        unit = " days";
+    } else if (diff > 3600) {
+        magnitude = diff / 3600;
+        unit = " hours";
+    } else if (diff > 60) {
+        magnitude = diff / 60;
+        unit = " minutes";
+    } else {
+        magnitude = diff;
+        unit = " seconds";
+    }
+    let txt;
+    if (time.classList.contains("start")) {
+        txt = (magnitude | 0) + unit + " ago";
+    } else {
+        txt = "After " + (magnitude | 0) + unit;
+    }
+    time.textContent = txt;
+}
+
+// Make whole row clickable
