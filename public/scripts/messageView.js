39da1216009191c3072a761bed4e11ba08d4142a
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-12 13:35:01 +0800

    Only random color for incoming msg

--- public/scripts/messageView.js
+++ public/scripts/messageView.js
@@ -1,4 +1,4 @@
-for (const author of document.querySelectorAll(".author")) {
+for (const author of document.querySelectorAll(".in > .author")) {
     const seed = [...author.textContent].map(a => a.charCodeAt(0)).reduce((a, b) => a+b);
     author.style.color = "#" + ((seed * 0xDEADBEEF % 0xFFFFFF) | 0).toString(16);
 }
