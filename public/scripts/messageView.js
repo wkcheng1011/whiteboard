586cf3fe0e9e88d24e298c6f0db958b25fb2f624
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-12 13:11:33 +0800

    Add autoscroll and enter-to-send

--- public/scripts/messageView.js
+++ public/scripts/messageView.js
@@ -2,3 +2,14 @@ for (const author of document.querySelectorAll(".author")) {
     const seed = [...author.textContent].map(a => a.charCodeAt(0)).reduce((a, b) => a+b);
     author.style.color = "#" + ((seed * 0xDEADBEEF % 0xFFFFFF) | 0).toString(16);
 }
+
+const messageDiv = document.querySelector("#message");
+messageDiv.scrollTop = messageDiv.scrollHeight;
+
+const textarea = document.querySelector("textarea[name=message]");
+textarea.addEventListener("keydown", (e) => {
+    if (e.keyCode == 13 && !e.shiftKey) {
+        e.preventDefault();
+        textarea.form.submit();
+    }
+})
