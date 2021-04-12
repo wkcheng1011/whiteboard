9976da5c8da5337fb77449e993d0ca035541f92f
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-13 01:45:43 +0800

    Fix error on messgae

--- routes/message.js
+++ routes/message.js
@@ -16,22 +16,34 @@ router.get("/", async (req, res) => {
 		const classes = await db.all("select * from classes");
 		const users = await db.all("select * from users");
 
-		const messages = (await db.all("select count(*) as cnt, * from messages group by to_id order by at desc")).filter(message => {
-			return message.type == 0 && message.to_id == req.session.user.id || message.type == 1
-		}).map(message => {
-			if (message.type == 0) { // DM
-				message.name = users.filter(user => user.id == message.from_id)[0].name;
-				message.target_id = message.from_id;
-			} else { // GM
-				message.name = classes.filter(_class => _class.id == message.to_id)[0].name;
-				message.target_id = message.to_id;
-			}
+		const counts = {};
+
+		const sentMessages = await db.all("select * from messages where from_id = ? and type = 0", req.session.user.id);
+		const receivedMessages = await db.all("select * from messages where to_id = ? and type = 0", req.session.user.id);
+
+		for (const message of sentMessages) {
+			counts[message.to_id] = counts[message.to_id] || message;
+			counts[message.to_id].name = users.filter(_user => _user.id == message.to_id)[0].name;
+			counts[message.to_id].target_id = message.to_id;
+			counts[message.to_id].cnt = (counts[message.to_id].cnt || 0) + 1;
+		}
+
+		for (const message of receivedMessages) {
+			counts[message.from_id] = counts[message.from_id] || message;
+			counts[message.from_id].name = users.filter(_user => _user.id == message.from_id)[0].name;
+			counts[message.from_id].target_id = message.from_id;
+			counts[message.from_id].cnt = (counts[message.from_id].cnt || 0) + 1;
+		}
+
+		const classMessages = (await db.all("select count(*) as cnt, * from messages where type = 1 group by to_id order by at desc")).map(message => {
+			message.name = classes.filter(_class => _class.id == message.to_id)[0].name;
+			message.target_id = message.to_id;
 			return message;
-		}).sort((m1, m2) => {
-			return new Date(m2.at) - new Date(m1.at);
 		});
 
-		return res.render("message", { messages });
+		return res.render("message", { messages: [...Object.values(counts), ...classMessages].sort((m1, m2) => {
+			return new Date(m2.at) - new Date(m1.at);
+		}) });
 	}
 });
 
@@ -60,29 +72,11 @@ router.post("/new", async (req, res) => {
 			return res.render("error", { message: "Not a valid UUID", error: to_id });
 		}
 
-        const result = await db.get(`select * from users where id = ?`, to_id);
-        
-        if (!result) {
-            return res.render("error", {
-				message: "User does not exist",
-				error: to_id
-			});
-        }
-
-        // Create channel
-        const channel_id = uuid();
-        await db.run(`insert into channels values (?)`, channel_id);
-        
-        // Add the 2 users to channel
-        for (const user_id of [from_id, to_id]) {
-            await db.run("insert into participants values (?, ?)", channel_id, user_id);
-        }
-
         // Add the message
         const message_id = uuid();
-        await db.run("insert into messages (id, user_id, channel_id, title, content) values (?, ?, ?, ?, ?)", message_id, from_id, channel_id, req.body.title, req.body.content);
+        await db.run("insert into messages (id, from_id, to_id, type, content) values (?, ?, ?, ?, ?)", message_id, from_id, to_id, 0, req.body.content);
         
-		res.redirect("/message");
+		res.redirect("/messages/0/" + to_id);
 	}
 });
 
@@ -104,7 +98,7 @@ router.get(/\/(\d)\/(.{36})/, async (req, res) => {
 
 		let messages;
 		if (type == 0) { // DM
-			messages = await db.all("select users.name as displayname, * from messages, users where from_id = ? and from_id = users.id", id);
+			messages = await db.all("select users.name as displayname, * from messages, users where (from_id = ? and to_id = ? or from_id = ? and to_id = ?) and from_id = users.id", id, req.session.user.id, req.session.user.id, id);
 		} else { // GM
 			messages = await db.all("select classes.name as displayname, users.name as user_name, * from messages, classes, users where to_id = ? and to_id = classes.id and from_id = users.id", id);
 		}
