e7d65f53cfd15ad63a71afc94aba94ac395a71ed
Ivan Cheng <wkcheng1011@gmail.com>
2021-04-21 17:15:50 +0800

    Hardcode previous button on messageview

--- public/scripts/messageView.js
+++ public/scripts/messageView.js
@@ -1,3 +1,5 @@
+document.querySelector('#bk-btn').href = "/messages/";
+
 for (const author of document.querySelectorAll(".in > .author")) {
     const seed = [...author.textContent].map(a => a.charCodeAt(0)).reduce((a, b) => a+b);
     author.style.color = "#" + ((seed * 0xDEADBEEF % 0xFFFFFF) | 0).toString(16);
